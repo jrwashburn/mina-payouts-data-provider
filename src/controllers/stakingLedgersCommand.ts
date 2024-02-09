959e16087751634a206d909224be0179933a3888
Jonathan Washburn <jrwashburn@gmail.com>
2024-02-09 17:11:21 -0600

    add error handling to staking ledger upload

--- src/controllers/stakingLedgersCommand.ts
+++ src/controllers/stakingLedgersCommand.ts
@@ -2,35 +2,37 @@ import * as sldb from '../database/stakingLedgerDb';
 import { ControllerResponse } from '../models/controller';
 
 export async function uploadStakingLedger(file: Buffer, hash: string, nextEpoch: number | null) {
+  console.log('uploadStakingLedger called for hash:', hash);
   const isAlreadyImported = await sldb.hashExists(hash);
-  // eslint-disable-next-line prefer-const
-
   if (isAlreadyImported) {
+    console.log(`File with hash ${hash} was already imported`);
     const controllerResponse: ControllerResponse = { responseMessages: [`File with hash ${hash} was already imported`], responseCode: 409 }
     return controllerResponse;
   }
   else {
     try {
+      console.log(`Processing file ${hash}`);
       await loadData(file, hash, nextEpoch);
       console.log(`Finished processing file ${hash}`);
       const controllerResponse: ControllerResponse = { responseMessages: [`Received file and processing for hash ${hash}`] };
       return (controllerResponse);
     } catch (error) {
+      console.error('Error processing file:', error);
       const controllerResponse: ControllerResponse = { responseError: 'Error processing file', responseCode: 500 }
       return controllerResponse;
     }
   }
 }
 
-function loadData(ledger: Buffer, hash: string, nextepoch: number | null) {
+async function loadData(ledger: Buffer, hash: string, nextepoch: number | null) {
   console.log('loadData called');
-  const jsonData = ledger.toString('utf8');
-  console.log('finished waiting for chunks');
-  console.log('jsonData:', jsonData.slice(0, 100)); // log the first 100 characters
   try {
+    const jsonData = ledger.toString('utf8');
+    console.log('jsonData:', jsonData.slice(0, 100)); // log the first 100 characters
     const dataArray = JSON.parse(jsonData);
-    sldb.insertBatch(dataArray, hash, nextepoch);
+    await sldb.insertBatch(dataArray, hash, nextepoch);
   } catch (error) {
+    console.error('Error parsing json or inserting batch:', error);
     const controllerResponse: ControllerResponse = { responseError: 'Error parsing json or inserting batch', responseCode: 500 }
     return controllerResponse;
   }
